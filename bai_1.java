51c20497dace33b2615561fe50b07e281f255a29
Nguyen Huu Hung <hunghuu24@gmail.com>
2025-09-10 08:30:43 +0700

    Xoa bai_1.java

--- bai_1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class bai_1{
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Nhap ten cua ban: ");
-        String name = sc.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }}
